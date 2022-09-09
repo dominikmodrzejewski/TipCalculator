0953cd19cffd9d4764d9915e830b00f9c2930c54
justyna-skowronska <justyna.skowronska97@gmail.com>
2022-09-09 13:47:55 +0200

    style

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { MainComponent } from './main/main.component';
+import { CountComponent } from './count/count.component';
+import { ResultComponent } from './result/result.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MainComponent
+    MainComponent,
+    CountComponent,
+    ResultComponent
   ],
   imports: [
     BrowserModule
